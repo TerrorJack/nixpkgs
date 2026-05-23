1abc874c3da4f007357f1aa737ff69a92de4711c
Cheng Shao <terrorjack@type.dance>
2026-05-23 22:57:43 +0200

    WIP

--- pkgs/development/compilers/graalvm/graalvm-oracle/default.nix
+++ pkgs/development/compilers/graalvm/graalvm-oracle/default.nix
@@ -7,10 +7,38 @@
   version ? "25",
 }:
 
-graalvmPackages.buildGraalvm {
+let
+  updateScript =
+    if
+      builtins.elem version [
+        "17"
+        "25"
+      ]
+    then
+      {
+        command = [
+          ./update.sh
+          version
+        ];
+        attrPath =
+          if version == "25" then
+            "graalvmPackages.graalvm-oracle"
+          else
+            "graalvmPackages.graalvm-oracle_${version}";
+        supportedFeatures = [ "commit" ];
+      }
+    else
+      null;
+in
+(graalvmPackages.buildGraalvm {
   inherit useMusl version;
   src = fetchurl (import ./hashes.nix).${version}.${stdenv.system};
   meta.platforms = builtins.attrNames (import ./hashes.nix).${version};
   meta.license = lib.licenses.unfree;
   pname = "graalvm-oracle";
-}
+}).overrideAttrs
+  (previousAttrs: {
+    passthru = (previousAttrs.passthru or { }) // {
+      inherit updateScript;
+    };
+  })
